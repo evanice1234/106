2658347211f19ffd4e4923f2aa588ab188061212
evanice1234 <81807469+evanice1234@users.noreply.github.com>
2021-09-08 15:28:08 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true});
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/58q4viZs6/model.json', YAYProject);
+}
+
+function YAYProject(){
+  classifier.classify(gotResults);
+}
+
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    lol_red = Math.floor(Math.random() * 255) + 1;
+    bluee = Math.floor(Math.random() * 255) + 1;
+    funny_green = Math.floor(Math.random() * 255) + 1;
+
+    document.getElementById("result_label").innerHTML = 'Detected voice is of  - '+ results[0].label;
+    document.getElementById("result_count").innerHTML = "Acurracy of voice - "+ (results[0].confidence * 100).toFixed(2); + "%";
+    document.getElementById("result_label").style.color = 'rgb(' + lol_red + ',' +  funny_green +',' +  bluee+ ')'; 
+         document.getElementById("result_count").style.color = 'rgb(' + lol_red + ',' +  funny_green +',' +  bluee + ')';  
+    var doggo = document.getElementById('funny-animals-dog.gif');
+    var cat = document.getElementById('cute-kitty-best-kitty.gif');
+ 
+    if (results[0].label == "Dog") {
+      img.src = 'bark.gif';
+      doggo = doggo+1;
+    } else if (results[0].label == "Meow") {
+      img.src = 'meow.gif';
+      cat = cat + 1;
+    } else{
+      img.src = 'listen.gif';
+    }
+     }
+ }
+
